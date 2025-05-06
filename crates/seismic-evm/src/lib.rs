1fcd21b2c9ef7b5c4e03c8f30536f00ae5651f69
Spencer Solit <sol@seismic.systems>
2025-05-06 11:13:06 -0400

    modify lib.rs

--- crates/seismic-evm/src/lib.rs
+++ crates/seismic-evm/src/lib.rs
@@ -9,113 +9,104 @@
 
 extern crate alloc;
 
-use alloc::vec::Vec;
+use alloy_evm::eth::EthEvmContext;
+use alloy_evm::IntoTxEnv;
 use alloy_evm::{Database, Evm, EvmEnv, EvmFactory};
-use alloy_primitives::{Address, Bytes, TxKind, U256};
-use core::{
-    fmt::Debug,
-    ops::{Deref, DerefMut},
-};
-use op_alloy_consensus::OpTxType;
-use op_revm::{
-    precompiles::OpPrecompiles, DefaultOp, OpBuilder, OpContext, OpHaltReason, OpSpecId,
-    OpTransaction, OpTransactionError,
-};
+use alloy_evm::EthEvm;
+use alloy_primitives::{Address, Bytes};
+use core::fmt::Debug;
+use revm::MainBuilder;
+use revm::MainContext;
 use revm::{
     context::{BlockEnv, TxEnv},
-    context_interface::result::{EVMError, ResultAndState},
-    handler::{instructions::EthInstructions, PrecompileProvider},
+    context_interface::result::{EVMError, HaltReason, ResultAndState},
+    context_interface::ContextTr,
+    handler::EthPrecompiles,
+    handler::PrecompileProvider,
     inspector::NoOpInspector,
     interpreter::{interpreter::EthInterpreter, InterpreterResult},
-    Context, ExecuteEvm, InspectEvm, Inspector,
+    primitives::hardfork::SpecId,
+    Context, Inspector,
 };
 
 pub mod block;
 pub use block::{OpBlockExecutionCtx, OpBlockExecutor, OpBlockExecutorFactory};
 
-/// OP EVM implementation.
+/// Seismic EVM implementation.
 ///
 /// This is a wrapper type around the `revm` evm with optional [`Inspector`] (tracing)
 /// support. [`Inspector`] support is configurable at runtime because it's part of the underlying
-/// [`OpEvm`](op_revm::OpEvm) type.
-#[allow(missing_debug_implementations)] // missing revm::OpContext Debug impl
-pub struct OpEvm<DB: Database, I, P = OpPrecompiles> {
-    inner: op_revm::OpEvm<OpContext<DB>, I, EthInstructions<EthInterpreter, OpContext<DB>>, P>,
-    inspect: bool,
+/// [`SeismicEvm`](seismic_revm::SeismicEvm) type.
+#[allow(missing_debug_implementations)]
+pub struct SeismicEvm<DB: Database, I, PRECOMPILE = EthPrecompiles> {
+    inner: EthEvm<DB, I, PRECOMPILE>,
 }
 
-impl<DB: Database, I, P> OpEvm<DB, I, P> {
-    /// Provides a reference to the EVM context.
-    pub const fn ctx(&self) -> &OpContext<DB> {
-        &self.inner.0.data.ctx
-    }
+// impl<DB: Database, I, P> SeismicEvm<DB, I, P> {
+//     /// Provides a reference to the EVM context.
+//     pub const fn ctx(&self) -> &SeismicContext<DB> {
+//         &self.inner.0.data.ctx
+//     }
 
-    /// Provides a mutable reference to the EVM context.
-    pub fn ctx_mut(&mut self) -> &mut OpContext<DB> {
-        &mut self.inner.0.data.ctx
-    }
+//     /// Provides a mutable reference to the EVM context.
+//     pub fn ctx_mut(&mut self) -> &mut SeismicContext<DB> {
+//         &mut self.inner.0.data.ctx
+//     }
 
-    /// Provides a mutable reference to the EVM inspector.
-    pub fn inspector_mut(&mut self) -> &mut I {
-        &mut self.inner.0.data.inspector
-    }
-}
+//     /// Provides a mutable reference to the EVM inspector.
+//     pub fn inspector_mut(&mut self) -> &mut I {
+//         &mut self.inner.0.data.inspector
+//     }
+// }
 
-impl<DB: Database, I, P> OpEvm<DB, I, P> {
-    /// Creates a new OP EVM instance.
-    ///
-    /// The `inspect` argument determines whether the configured [`Inspector`] of the given
-    /// [`OpEvm`](op_revm::OpEvm) should be invoked on [`Evm::transact`].
-    pub const fn new(
-        evm: op_revm::OpEvm<OpContext<DB>, I, EthInstructions<EthInterpreter, OpContext<DB>>, P>,
-        inspect: bool,
-    ) -> Self {
-        Self { inner: evm, inspect }
+impl<DB: Database, I, PRECOMPILE> SeismicEvm<DB, I, PRECOMPILE> {
+    pub fn new(inner: EthEvm<DB, I, PRECOMPILE>) -> Self {
+        Self { inner }
     }
 }
 
-impl<DB: Database, I, P> Deref for OpEvm<DB, I, P> {
-    type Target = OpContext<DB>;
+// impl<DB: Database, I, P> Deref for OpEvm<DB, I, P> {
+//     type Target = OpContext<DB>;
 
-    #[inline]
-    fn deref(&self) -> &Self::Target {
-        self.ctx()
-    }
-}
+//     #[inline]
+//     fn deref(&self) -> &Self::Target {
+//         self.ctx()
+//     }
+// }
 
-impl<DB: Database, I, P> DerefMut for OpEvm<DB, I, P> {
-    #[inline]
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.ctx_mut()
-    }
-}
+// impl<DB: Database, I, P> DerefMut for OpEvm<DB, I, P> {
+//     #[inline]
+//     fn deref_mut(&mut self) -> &mut Self::Target {
+//         self.ctx_mut()
+//     }
+// }
 
-impl<DB, I, P> Evm for OpEvm<DB, I, P>
+impl<DB, I, PRECOMPILE> Evm for SeismicEvm<DB, I, PRECOMPILE>
 where
     DB: Database,
-    I: Inspector<OpContext<DB>>,
-    P: PrecompileProvider<OpContext<DB>, Output = InterpreterResult>,
+    I: Inspector<EthEvmContext<DB>>,
+    PRECOMPILE: PrecompileProvider<EthEvmContext<DB>, Output = InterpreterResult>,
 {
     type DB = DB;
-    type Tx = OpTransaction<TxEnv>;
-    type Error = EVMError<DB::Error, OpTransactionError>;
-    type HaltReason = OpHaltReason;
-    type Spec = OpSpecId;
+    type Tx = TxEnv;
+    type Error = EVMError<DB::Error>;
+    type HaltReason = HaltReason;
+    type Spec = SpecId;
 
     fn block(&self) -> &BlockEnv {
-        &self.block
+        self.inner.block()
     }
 
-    fn transact_raw(
+    fn transact_raw(&mut self, tx: Self::Tx) -> Result<ResultAndState, Self::Error> {
+        self.inner.transact_raw(tx)
+    }
+
+    fn transact(
         &mut self,
-        tx: Self::Tx,
+        tx: impl IntoTxEnv<Self::Tx>,
     ) -> Result<ResultAndState<Self::HaltReason>, Self::Error> {
-        if self.inspect {
-            self.inner.set_tx(tx);
-            self.inner.inspect_replay()
-        } else {
-            self.inner.transact(tx)
-        }
+        // attention: ENCRYPT and DECRYPT HERE
+        self.transact_raw(tx.into_tx_env())
     }
 
     fn transact_system_call(
@@ -123,128 +114,99 @@ where
         caller: Address,
         contract: Address,
         data: Bytes,
-    ) -> Result<ResultAndState<Self::HaltReason>, Self::Error> {
-        let tx = OpTransaction {
-            base: TxEnv {
-                caller,
-                kind: TxKind::Call(contract),
-                // Explicitly set nonce to 0 so revm does not do any nonce checks
-                nonce: 0,
-                gas_limit: 30_000_000,
-                value: U256::ZERO,
-                data,
-                // Setting the gas price to zero enforces that no value is transferred as part of
-                // the call, and that the call will not count against the block's
-                // gas limit
-                gas_price: 0,
-                // The chain ID check is not relevant here and is disabled if set to None
-                chain_id: None,
-                // Setting the gas priority fee to None ensures the effective gas price is derived
-                // from the `gas_price` field, which we need to be zero
-                gas_priority_fee: None,
-                access_list: Default::default(),
-                // blob fields can be None for this tx
-                blob_hashes: Vec::new(),
-                max_fee_per_blob_gas: 0,
-                tx_type: OpTxType::Deposit as u8,
-                authorization_list: Default::default(),
-            },
-            // The L1 fee is not charged for the EIP-4788 transaction, submit zero bytes for the
-            // enveloped tx size.
-            enveloped_tx: Some(Bytes::default()),
-            deposit: Default::default(),
-        };
-
-        let mut gas_limit = tx.base.gas_limit;
-        let mut basefee = 0;
-        let mut disable_nonce_check = true;
-
-        // ensure the block gas limit is >= the tx
-        core::mem::swap(&mut self.block.gas_limit, &mut gas_limit);
-        // disable the base fee check for this call by setting the base fee to zero
-        core::mem::swap(&mut self.block.basefee, &mut basefee);
-        // disable the nonce check
-        core::mem::swap(&mut self.cfg.disable_nonce_check, &mut disable_nonce_check);
-
-        let mut res = self.transact(tx);
-
-        // swap back to the previous gas limit
-        core::mem::swap(&mut self.block.gas_limit, &mut gas_limit);
-        // swap back to the previous base fee
-        core::mem::swap(&mut self.block.basefee, &mut basefee);
-        // swap back to the previous nonce check flag
-        core::mem::swap(&mut self.cfg.disable_nonce_check, &mut disable_nonce_check);
-
-        // NOTE: We assume that only the contract storage is modified. Revm currently marks the
-        // caller and block beneficiary accounts as "touched" when we do the above transact calls,
-        // and includes them in the result.
-        //
-        // We're doing this state cleanup to make sure that changeset only includes the changed
-        // contract storage.
-        if let Ok(res) = &mut res {
-            res.state.retain(|addr, _| *addr == contract);
-        }
-
-        res
+    ) -> Result<ResultAndState, Self::Error> {
+        self.inner.transact_system_call(caller, contract, data)
     }
 
     fn db_mut(&mut self) -> &mut Self::DB {
-        &mut self.journaled_state.database
+        self.inner.db_mut()
     }
 
     fn finish(self) -> (Self::DB, EvmEnv<Self::Spec>) {
-        let Context { block: block_env, cfg: cfg_env, journaled_state, .. } = self.inner.0.data.ctx;
-
-        (journaled_state.database, EvmEnv { block_env, cfg_env })
+        self.inner.finish()
     }
 
     fn set_inspector_enabled(&mut self, enabled: bool) {
-        self.inspect = enabled;
+        self.inner.set_inspector_enabled(enabled);
     }
 }
 
-/// Factory producing [`OpEvm`]s.
-#[derive(Debug, Default, Clone, Copy)]
+/// Custom EVM configuration.
+#[derive(Debug, Clone, Default)]
 #[non_exhaustive]
-pub struct OpEvmFactory;
+pub struct SeismicEvmFactory;
 
-impl EvmFactory for OpEvmFactory {
-    type Evm<DB: Database, I: Inspector<OpContext<DB>>> = OpEvm<DB, I>;
-    type Context<DB: Database> = OpContext<DB>;
-    type Tx = OpTransaction<TxEnv>;
-    type Error<DBError: core::error::Error + Send + Sync + 'static> =
-        EVMError<DBError, OpTransactionError>;
-    type HaltReason = OpHaltReason;
-    type Spec = OpSpecId;
+impl EvmFactory for SeismicEvmFactory {
+    type Evm<DB: Database, I: Inspector<EthEvmContext<DB>, EthInterpreter>> =
+        SeismicEvm<DB, I, CustomPrecompiles>;
+    type Tx = TxEnv;
+    type Error<DBError: core::error::Error + Send + Sync + 'static> = EVMError<DBError>;
+    type HaltReason = HaltReason;
+    type Context<DB: Database> = EthEvmContext<DB>;
+    type Spec = SpecId;
 
-    fn create_evm<DB: Database>(
-        &self,
-        db: DB,
-        input: EvmEnv<OpSpecId>,
-    ) -> Self::Evm<DB, NoOpInspector> {
-        OpEvm {
-            inner: Context::op()
-                .with_db(db)
-                .with_block(input.block_env)
-                .with_cfg(input.cfg_env)
-                .build_op_with_inspector(NoOpInspector {}),
-            inspect: false,
-        }
+    fn create_evm<DB: Database>(&self, db: DB, input: EvmEnv) -> Self::Evm<DB, NoOpInspector> {
+        let evm = Context::mainnet()
+            .with_db(db)
+            .with_cfg(input.cfg_env)
+            .with_block(input.block_env)
+            .build_mainnet_with_inspector(NoOpInspector {})
+            .with_precompiles(CustomPrecompiles::new());
+
+        let ethevm = EthEvm::new(evm, false);
+        SeismicEvm::new(ethevm)
     }
 
-    fn create_evm_with_inspector<DB: Database, I: Inspector<Self::Context<DB>>>(
+    fn create_evm_with_inspector<DB: Database, I: Inspector<Self::Context<DB>, EthInterpreter>>(
         &self,
         db: DB,
-        input: EvmEnv<OpSpecId>,
+        input: EvmEnv,
         inspector: I,
     ) -> Self::Evm<DB, I> {
-        OpEvm {
-            inner: Context::op()
-                .with_db(db)
-                .with_block(input.block_env)
-                .with_cfg(input.cfg_env)
-                .build_op_with_inspector(inspector),
-            inspect: true,
-        }
+        let ethevm = EthEvm::new(
+            self.create_evm(db, input).inner.into_inner().with_inspector(inspector),
+            true,
+        );
+        SeismicEvm::new(ethevm)
+    }
+}
+
+/// A custom precompile that contains static precompiles.
+#[derive(Clone)]
+pub struct CustomPrecompiles {
+    pub precompiles: EthPrecompiles,
+}
+
+impl CustomPrecompiles {
+    /// Given a [`PrecompileProvider`] and cache for a specific precompiles, create a
+    /// wrapper that can be used inside Evm.
+    fn new() -> Self {
+        Self { precompiles: EthPrecompiles::default() }
+    }
+}
+
+impl<CTX: ContextTr> PrecompileProvider<CTX> for CustomPrecompiles {
+    type Output = InterpreterResult;
+
+    fn set_spec(&mut self, spec: <CTX::Cfg as revm::context::Cfg>::Spec) {
+        self.precompiles.set_spec(spec)
+    }
+
+    fn run(
+        &mut self,
+        _context: &mut CTX,
+        address: &Address,
+        bytes: &Bytes,
+        gas_limit: u64,
+    ) -> Result<Option<InterpreterResult>, String> {
+        self.precompiles.run(_context, address, bytes, gas_limit)
+    }
+
+    fn warm_addresses(&self) -> Box<impl Iterator<Item = Address>> {
+        self.precompiles.warm_addresses()
+    }
+
+    fn contains(&self, address: &Address) -> bool {
+        self.precompiles.contains(address)
     }
 }
