c6683099db30d64a9bc70a049f5fef7812c86908
Spencer Solit <sol@seismic.systems>
2025-05-07 12:19:32 -0400

    fix transact_system_call

--- crates/seismic-evm/src/lib.rs
+++ crates/seismic-evm/src/lib.rs
@@ -35,8 +35,10 @@ use revm::ExecuteEvm;
 use seismic_revm::SeismicHaltReason;
 use seismic_revm::transaction::abstraction::SeismicTransaction;
 use seismic_revm::SeismicBuilder;
-use alloy_primitives::U256;
+use alloy_primitives::{B256, U256};
 use alloy_primitives::TxKind;
+use seismic_revm::transaction::abstraction::RngMode;
+
 
 /// Seismic EVM implementation.
 ///
@@ -129,7 +131,66 @@ where
         contract: Address,
         data: Bytes,
     ) -> Result<ResultAndState<Self::HaltReason>, Self::Error> {
-        self.inner.transact_system_call(caller, contract, data)
+        let tx = SeismicTransaction {
+            base: TxEnv {
+                caller,
+                kind: TxKind::Call(contract),
+                // Explicitly set nonce to 0 so revm does not do any nonce checks
+                nonce: 0,
+                gas_limit: 30_000_000,
+                value: U256::ZERO,
+                data,
+                // Setting the gas price to zero enforces that no value is transferred as part of
+                // the call, and that the call will not count against the block's
+                // gas limit
+                gas_price: 0,
+                // The chain ID check is not relevant here and is disabled if set to None
+                chain_id: None,
+                // Setting the gas priority fee to None ensures the effective gas price is derived
+                // from the `gas_price` field, which we need to be zero
+                gas_priority_fee: None,
+                access_list: Default::default(),
+                // blob fields can be None for this tx
+                blob_hashes: Vec::new(),
+                max_fee_per_blob_gas: 0,
+                tx_type: 0,
+                authorization_list: Default::default(),
+            },
+            tx_hash: B256::ZERO,
+            rng_mode: RngMode::Execution,
+        };
+
+        let mut gas_limit = tx.base.gas_limit;
+        let mut basefee = 0;
+        let mut disable_nonce_check = true;
+
+        // ensure the block gas limit is >= the tx
+        core::mem::swap(&mut self.block.gas_limit, &mut gas_limit);
+        // disable the base fee check for this call by setting the base fee to zero
+        core::mem::swap(&mut self.block.basefee, &mut basefee);
+        // disable the nonce check
+        core::mem::swap(&mut self.cfg.disable_nonce_check, &mut disable_nonce_check);
+
+        let mut res = self.transact(tx);
+
+        // swap back to the previous gas limit
+        core::mem::swap(&mut self.block.gas_limit, &mut gas_limit);
+        // swap back to the previous base fee
+        core::mem::swap(&mut self.block.basefee, &mut basefee);
+        // swap back to the previous nonce check flag
+        core::mem::swap(&mut self.cfg.disable_nonce_check, &mut disable_nonce_check);
+
+        // NOTE: We assume that only the contract storage is modified. Revm currently marks the
+        // caller and block beneficiary accounts as "touched" when we do the above transact calls,
+        // and includes them in the result.
+        //
+        // We're doing this state cleanup to make sure that changeset only includes the changed
+        // contract storage.
+        if let Ok(res) = &mut res {
+            res.state.retain(|addr, _| *addr == contract);
+        }
+
+        res
     }
 
     fn db_mut(&mut self) -> &mut Self::DB {
