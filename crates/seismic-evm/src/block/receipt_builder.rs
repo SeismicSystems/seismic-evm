20469bf4a5a674aea6f7acad6bb0601fe66c7220
Spencer Solit <sol@seismic.systems>
2025-05-23 10:09:43 -0400

    seismic block executor uses an inner eth executor

--- crates/seismic-evm/src/block/receipt_builder.rs
+++ crates/seismic-evm/src/block/receipt_builder.rs
@@ -1,39 +1,23 @@
+//! Custom receipt builder for Seismic.
+
 use alloy_consensus::Eip658Value;
 use alloy_evm::{eth::receipt_builder::ReceiptBuilderCtx, Evm};
 use seismic_alloy_consensus::{SeismicReceiptEnvelope, SeismicTxEnvelope, SeismicTxType};
-
-/// Type that knows how to build a receipt based on execution result.
-#[auto_impl::auto_impl(&, Arc)]
-pub trait SeismicReceiptBuilder {
-    /// Transaction type.
-    type Transaction;
-    /// Receipt type.
-    type Receipt;
-
-    /// Builds a receipt given a transaction and the result of the execution.
-    ///
-    /// Note: this method should return `Err` if the transaction is a deposit transaction. In that
-    /// case, the `build_deposit_receipt` method will be called.
-    #[expect(clippy::result_large_err)] // Err(_) is always consumed
-    fn build_receipt<'a, E: Evm>(
-        &self,
-        ctx: ReceiptBuilderCtx<'a, Self::Transaction, E>,
-    ) -> Result<Self::Receipt, ReceiptBuilderCtx<'a, Self::Transaction, E>>;
-}
+use alloy_evm::eth::receipt_builder::ReceiptBuilder;
 
 /// Receipt builder operating on op-alloy types.
 #[derive(Debug, Default, Clone, Copy)]
 #[non_exhaustive]
 pub struct SeismicAlloyReceiptBuilder;
 
-impl SeismicReceiptBuilder for SeismicAlloyReceiptBuilder {
+impl ReceiptBuilder for SeismicAlloyReceiptBuilder {
     type Transaction = SeismicTxEnvelope;
     type Receipt = SeismicReceiptEnvelope;
 
     fn build_receipt<'a, E: Evm>(
         &self,
         ctx: ReceiptBuilderCtx<'a, Self::Transaction, E>,
-    ) -> Result<Self::Receipt, ReceiptBuilderCtx<'a, Self::Transaction, E>> {
+    ) -> Self::Receipt {
         let receipt_with_bloom = alloy_consensus::Receipt {
             status: Eip658Value::Eip658(ctx.result.is_success()),
             cumulative_gas_used: ctx.cumulative_gas_used,
@@ -43,7 +27,7 @@ impl SeismicReceiptBuilder for SeismicAlloyReceiptBuilder {
 
         let ty = ctx.tx.tx_type();
 
-        Ok(match ty {
+        match ty {
             SeismicTxType::Legacy => SeismicReceiptEnvelope::Legacy(receipt_with_bloom),
             SeismicTxType::Eip2930 => SeismicReceiptEnvelope::Eip2930(receipt_with_bloom),
             SeismicTxType::Eip1559 => SeismicReceiptEnvelope::Eip1559(receipt_with_bloom),
@@ -51,6 +35,6 @@ impl SeismicReceiptBuilder for SeismicAlloyReceiptBuilder {
             SeismicTxType::Seismic => SeismicReceiptEnvelope::Seismic(receipt_with_bloom),
             #[allow(unreachable_patterns)]
             _ => unreachable!(),
-        })
+        }
     }
 }
